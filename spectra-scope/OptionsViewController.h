4b85dc7d5d1135702e98f1f9511370e081dcdba6
ultramailman <cyrus9212@gmail.com>
2013-11-20 16:03:29 -0800

    changed project files organization, created options view controller and linked to storyboard

---
+++ spectra-scope/OptionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  OptionsViewController.h
+//  spectra-scope
+//
+//  Created by Tian Lin Tan on 13-11-20.
+//  Copyright (c) 2013 spectra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OptionsViewController : UIViewController
+
+@end
